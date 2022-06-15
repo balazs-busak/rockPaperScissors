fb01a6188508c32a6a53e16feb33a1beac3f6539
busjee <busakbalazs@gmail.com>
2022-06-15 09:16:22 +0100

    Computer choice

---
+++ scirpt.js
@@ -0,0 +1,30 @@
+
+// const choices = ["rock", "papaer", "scissors"]
+
+// function computerChoice() {
+//     return choices [Math.floor(Math.random() * choices.length)];
+// }
+
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
+
+// // let randNum = 3
+// let rand = Math.floor(Math.random()* 3) + 1;
+
+// console.log(rand);
+
+function computerPlay() {
+ 
+    let rand = Math.floor(Math.random()* 3) + 1;
+
+    if (rand = 1) {
+        console.log(rock);
+    } else if (rand = 2) {
+        console.log(paper);
+    
+    } else {
+        console.log(scissors);
+    }
+}
+
