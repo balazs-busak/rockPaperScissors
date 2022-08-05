27d33489782de31ead3dc15d0c56badd55476203
busjee <busakbalazs@gmail.com>
2022-08-05 02:53:39 +0100

    Added click event, and print outcomes

--- scirpt.js
+++ scirpt.js
@@ -5,6 +5,8 @@ let computerScore = 0
 const rockButton = document.querySelector('.rock')
 const paperButton = document.querySelector('.paper')
 const scissorsButton = document.querySelector('.scissors')
+const gameContainer = document.querySelector('.container');
+const results = document.querySelector('.results');
 
 // Get a random string from an array as Computer's pick.
 
@@ -15,74 +17,103 @@ function computerPlay() {
     return computerChoice
 }
 
+// const results = document.createElement('div');
+// results.classList.add('results');
+// results.style.margin = '20px';
+// // results.textContent = '';
+// gameContainer.appendChild(results);
+
+// Play a round that returns a string after setting Conditional Statements.
+
+function playRound (playerSelection, computerSelection) {
+    console.log('1 ', playerSelection, '2 ', computerSelection)
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        const p = document.createElement('p');
+        p.innerText = "It's a tie, You both picked ROCK!";
+        results.appendChild(p);
+    }   else if (playerSelection === "paper" && computerSelection === "paper") {
+        const p = document.createElement('p');
+        p.innerText = "It's a tie, You both picked PAPER!"
+        results.appendChild(p);
+    }   else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        const p = document.createElement('p');
+        p.innerText = "It's a tie, You both picked SCISSORS!"
+        results.appendChild(p);
+    }   else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
+        const p = document.createElement('p');
+        p.innerText = "You win, ROCK beats SCISSORS!"
+        results.appendChild(p);
+    }   else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
+        const p = document.createElement('p');
+        p.innerText = "You lost, PAPER beats ROCK!"
+        results.appendChild(p);
+    }   else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
+        const p = document.createElement('p');
+        p.innerText = "You win, PAPER beats ROCK!"
+        results.appendChild(p);
+    }   else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
+        const p = document.createElement('p');
+        p.innerText = "You lost, SCISSORS beats PAPER!"
+        results.appendChild(p);
+    }   else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
+        const p = document.createElement('p');
+        p.innerText = "You win, SCISSORS beats PAPER!"
+        results.appendChild(p);
+    }   else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
+        const p = document.createElement('p');
+        p.innerText = "You lost, ROCK beats SCISSORS!"
+        results.appendChild(p);
+    }   
+}
+
+
 // When clicking a button it gives the playerSelection the same value while also returns a cumputerChoice
 
 rockButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'rock'
-    console.log(playerSelection)
-    console.log(computerPlay())
+    // console.log(playerSelection)
+    // console.log(computerPlay())
+    playRound(playerSelection, computerSelection);
 })
 
 paperButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'paper'
-    //console.log(playerSelection)
-    //console.log(computerPlay())
+    // console.log(playerSelection)
+    // console.log(computerPlay())
+    playRound(playerSelection, computerSelection);
 })
 
 scissorsButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const playerSelection = 'scissors'
-    //console.log(playerSelection)
-    //console.log(computerPlay())
+    // console.log(playerSelection)
+    // console.log(computerPlay())
+    playRound(playerSelection, computerSelection);
 })
 
+const scores = document.createElement('p');
+scores.innerText = 'Scores: Player: ' + playerScore;
+results.appendChild(scores);
 
-
-
-// Play a round that returns a string after setting Conditional Statements.
-
-function playRound (playerSelection, computerSelection) {
-    
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        return "It's a tie, You both picked ROCK!"
-    }   else if (playerSelection === "paper" && computerSelection === "paper") {
-        return "It's a tie, You both picked PAPER!"
-    }   else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "It's a tie, You both picked SCISSORS!"
-    }   else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++
-        return "You win, ROCK beats SCISSORS!"
-    }   else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++
-        return "You lost, PAPER beats ROCK!"
-    }   else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++
-        return "You win, PAPER beats ROCK!"
-    }   else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore++
-        return "You lost, SCISSORS beats PAPER!"
-    }   else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore++
-        return "You win, SCISSORS beats PAPER!"
-    }   else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++
-        return "You lost, ROCK beats SCISSORS!"
-    }   
-}
-
-// A for loop to play 5 rounds with comp and player added to the loop so it returns different each round.
+// // A for loop to play 5 rounds with comp and player added to the loop so it returns different each round.
 
 // function game() {
 //     for (let i = 0; i < 5; i++) {
-//         const playerSelection = prompt("Make your decision: ROCK, PAPER, SCISSORS. Choose by typing it down.").toLowerCase(); // This makes the prompt insensitive for any case
-//         const computerSelection = computerPlay();
-//         playRound(playerSelection, computerSelection)
-//         console.log(playRound(playerSelection, computerSelection))
+//         // const playerSelection = prompt("Make your decision: ROCK, PAPER, SCISSORS. Choose by typing it down.").toLowerCase(); // This makes the prompt insensitive for any case
+//         // const computerSelection = computerPlay();
+//         // playRound(playerSelection, computerSelection)
+//         // console.log(playRound(playerSelection, computerSelection))
 //     }
 
-//Return a string with the outcome of your game.
+// // Return a string with the outcome of your game.
 
 //     if (playerScore > computerScore) {
 //         return "You won the game this time, good job!"
